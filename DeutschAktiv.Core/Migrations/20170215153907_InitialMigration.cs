e4b89e2e82c6988cb955f25436390fc9a1a01ece
Tannheuser <aleksey.ryabinin@gmail.com>
2017-02-16 14:36:12 +0300

    Remove rider settings

---
+++ DeutschAktiv.Core/Migrations/20170215153907_InitialMigration.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DeutschAktiv.Core.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clubs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Action = table.Column<string>(nullable: true),
+                    Controller = table.Column<string>(nullable: true),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Description = table.Column<string>(nullable: true),
+                    Enabled = table.Column<bool>(nullable: false),
+                    IconClass = table.Column<string>(nullable: true),
+                    ImageUrl = table.Column<string>(nullable: true),
+                    Modified = table.Column<DateTime>(nullable: false),
+                    Subtitle = table.Column<string>(nullable: true),
+                    Summary = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clubs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Courses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Day = table.Column<string>(nullable: true),
+                    Enabled = table.Column<bool>(nullable: false),
+                    Level = table.Column<string>(nullable: true),
+                    Modified = table.Column<DateTime>(nullable: false),
+                    Price = table.Column<int>(nullable: false),
+                    Subtitle = table.Column<string>(nullable: true),
+                    Time = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Courses", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Schedule",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Created = table.Column<DateTime>(nullable: false),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Day = table.Column<string>(nullable: true),
+                    Enabled = table.Column<bool>(nullable: false),
+                    Modified = table.Column<DateTime>(nullable: false),
+                    Place = table.Column<string>(nullable: true),
+                    Price = table.Column<int>(nullable: false),
+                    Subtitle = table.Column<string>(nullable: true),
+                    Teacher = table.Column<string>(nullable: true),
+                    Time = table.Column<string>(nullable: true),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schedule", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clubs");
+
+            migrationBuilder.DropTable(
+                name: "Courses");
+
+            migrationBuilder.DropTable(
+                name: "Schedule");
+        }
+    }
+}
---
+++ DeutschAktiv.Core/Migrations/DataContextModelSnapshot.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using DeutschAktiv.Core.Models;
+
+namespace DeutschAktiv.Core.Migrations
+{
+    [DbContext(typeof(DataContext))]
+    partial class DataContextModelSnapshot : ModelSnapshot
+    {
+        protected override void BuildModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.0-rtm-22752");
+
+            modelBuilder.Entity("DeutschAktiv.Core.Models.Club", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Action");
+
+                    b.Property<string>("Controller");
+
+                    b.Property<DateTime>("Created");
+
+                    b.Property<string>("Description");
+
+                    b.Property<bool>("Enabled");
+
+                    b.Property<string>("IconClass");
+
+                    b.Property<string>("ImageUrl");
+
+                    b.Property<DateTime>("Modified");
+
+                    b.Property<string>("Subtitle");
+
+                    b.Property<string>("Summary");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Clubs");
+                });
+
+            modelBuilder.Entity("DeutschAktiv.Core.Models.Course", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("Created");
+
+                    b.Property<string>("Day");
+
+                    b.Property<bool>("Enabled");
+
+                    b.Property<string>("Level");
+
+                    b.Property<DateTime>("Modified");
+
+                    b.Property<int>("Price");
+
+                    b.Property<string>("Subtitle");
+
+                    b.Property<string>("Time");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Courses");
+                });
+
+            modelBuilder.Entity("DeutschAktiv.Core.Models.ScheduleItem", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<DateTime>("Created");
+
+                    b.Property<DateTime>("Date");
+
+                    b.Property<string>("Day");
+
+                    b.Property<bool>("Enabled");
+
+                    b.Property<DateTime>("Modified");
+
+                    b.Property<string>("Place");
+
+                    b.Property<int>("Price");
+
+                    b.Property<string>("Subtitle");
+
+                    b.Property<string>("Teacher");
+
+                    b.Property<string>("Time");
+
+                    b.Property<string>("Title");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Schedule");
+                });
+        }
+    }
+}
