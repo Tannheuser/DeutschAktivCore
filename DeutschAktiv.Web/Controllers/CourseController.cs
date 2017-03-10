9ab33736c2df6d36c91524a4e34ec122d5a99614
Tannheuser <aleksey.ryabinin@gmail.com>
2017-03-10 19:40:57 +0300

    Split Education controller

---
+++ DeutschAktiv.Web/Controllers/CourseController.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using DeutschAktiv.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DeutschAktiv.Web.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly CourseService _courseService;
+
+        public CourseController(CourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        [Route("courses")]
+        public async Task<IActionResult> GetCourses()
+        {
+            var courses = await _courseService.GetAllAsync();
+            return View("CourseList", courses);
+        }
+    }
+}
--- DeutschAktiv.Web/Controllers/EducationController.cs
+++ DeutschAktiv.Web/Controllers/EducationController.cs
@@ -8,43 +8,43 @@ namespace DeutschAktiv.Web.Controllers
 {
     public class EducationController : Controller
     {
-        private readonly IClubService _clubService;
-        private readonly IScheduleService _scheduleService;
-        private readonly CourseService _courseService;
+//        private readonly IClubService _clubService;
+//        private readonly IScheduleService _scheduleService;
+//        private readonly CourseService _courseService;
+//
+//        public EducationController(IClubService clubService, CourseService courseService, IScheduleService scheduleService)
+//        {
+//            _clubService = clubService;
+//            _courseService = courseService;
+//            _scheduleService = scheduleService;
+//        }
 
-        public EducationController(IClubService clubService, CourseService courseService, IScheduleService scheduleService)
-        {
-            _clubService = clubService;
-            _courseService = courseService;
-            _scheduleService = scheduleService;
-        }
+//        [Route("courses")]
+//        public async Task<IActionResult> GetCourses()
+//        {
+//            var courses = await _courseService.GetAllAsync();
+//            return View("Courses", courses);
+//        }
 
-        [Route("courses")]
-        public async Task<IActionResult> GetCourses()
-        {
-            var courses = await _courseService.GetAllAsync();
-            return View("Courses", courses);
-        }
+//        [Route("clubs")]
+//        public async Task<IActionResult> GetClubs()
+//        {
+//            var clubs = await _clubService.GetClubsAsync();
+//            return View("Clubs", clubs);
+//        }
 
-        [Route("clubs")]
-        public async Task<IActionResult> GetClubs()
-        {
-            var clubs = await _clubService.GetClubsAsync();
-            return View("Clubs", clubs);
-        }
+//        [Route("masters")]
+//        public async Task<IActionResult> GetMasterClasses()
+//        {
+//            var masterClasses = await _clubService.GetMasterClassesAsync();
+//            return View("MasterClasses", masterClasses);
+//        }
 
-        [Route("masters")]
-        public async Task<IActionResult> GetMasterClasses()
-        {
-            var masterClasses = await _clubService.GetMasterClassesAsync();
-            return View("MasterClasses", masterClasses);
-        }
-
-        [Route("schedule")]
-        public async Task<IActionResult> GetSchedule()
-        {
-            var schedule = await _scheduleService.GetCurrentAsync();
-            return View("Schedule", schedule.OrderBy(s => s.Date));
-        }
+//        [Route("schedule")]
+//        public async Task<IActionResult> GetSchedule()
+//        {
+//            var schedule = await _scheduleService.GetCurrentAsync();
+//            return View("Schedule", schedule.OrderBy(s => s.Date));
+//        }
     }
 }
